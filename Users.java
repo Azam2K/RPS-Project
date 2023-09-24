e0adbf165eb2fa9dc29c23b8d115b7dd4f19a301
Azam <azamuseless@gmail.com>
2023-09-24 15:56:49 +0100

    Added reading in from file and creating users from File. Added comments to explain

--- Users.java
+++ Users.java
@@ -1,26 +1,37 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Users {
-    private ArrayList<User> myUsers;
-    Scanner passwordScanner;
+    private ArrayList<User> myUsers; // Arraylist to hold user objects, this will hold our players
+    private Scanner passwordScanner; // this scanner will take input from the user to delete their account if they wish
+    private Scanner fileScanner; // this scanner reads the players from the text file and places them into our myUsers arraylist which holds our players
+    private File txtFile; //this is just initialising our text File holding our players
 
-public Users(){
-myUsers = new ArrayList<User>();
-passwordScanner = new Scanner(System.in);
-//here i want to create a while loop that loads the arraylist with the info from the text file, keeps creating new objects with info from the file
+public Users(){//constructor
+myUsers = new ArrayList<User>();//initialising our arraylist of players that we're holding
+passwordScanner = new Scanner(System.in);//initialising our scanner from above
 
-//when loading a player in game class, i want to use find player and then set the current player to the object returned by the method, else say it doesnt exist
-//in game class, give user option to play AI, set winning score and if they want to play friend but both need to be logged in
-//try and introduce a wager system
-//introduce gui
-//introduce cool effects on win condition
-//add feedback system
+txtFile = new File("/Users/azam/Desktop/RPS-Project/Players.txt");//initialising our file so that it has our path and the scanner can read it
+try {
+    fileScanner = new Scanner(txtFile);//our scanner scans the txt file ready to be inputted into the list
+    while (fileScanner.hasNext()){//loop for our scanner to constantly read from the file until its empty
+        String username = fileScanner.next();//local variable to store the username from the file so it can get inputted into the list of our users
+        String password = fileScanner.next();//local variable to store the password from the file so it can get inputted into the list of our users
+        int score = fileScanner.nextInt();//local variable to store the score from the file so it can get inputted into the list of our users
+        int wins = fileScanner.nextInt();//local variable to store the winx from the file so it can get inputted into the list of our users
+        User importedUser = new User(username, password, score, wins);//finally creating a new user from a line of the file
+        myUsers.add(importedUser);//the user being added to our list of users
+    }
+} catch (FileNotFoundException e) {
+    System.out.println("File not found, please check your path is correct or if the file exists.");//if file doesn't exist, we say whats wrong
+}
 }
 
 public Boolean addPlayer(User newUser){//searches the arraylist of players and checks if the name exists, if it doesn't add this new object to the arraylist of players
     for (int i = 0; i < myUsers.size(); i++){
-       if(newUser.getUserName().equals(myUsers.get(i).getUserName())){
+       if(newUser.getUserName().toLowerCase().equals(myUsers.get(i).getUserName().toLowerCase())){
         return false;
        }
     }
@@ -28,9 +39,9 @@ public Boolean addPlayer(User newUser){//searches the arraylist of players and c
     return true;
 }
 
-public Boolean deletePlayer(User deleteUser){//maybe change this into the game class and have it as an option when the user is already logged in, just make them verify their password
+public Boolean deletePlayer(User deleteUser){//allows the user to delete their account if they so wish, requires them to enter their password
     for (User i : myUsers){
-        if (deleteUser.getUserName().equals(i.getUserName())){
+        if (deleteUser.getUserName().toLowerCase().equals(i.getUserName().toLowerCase())){
             System.out.println("Please enter your password to make sure you want to delete this account");
             if(i.getUserPassword().equals(passwordScanner.next())){
                 myUsers.remove(i);
@@ -44,7 +55,7 @@ public Boolean deletePlayer(User deleteUser){//maybe change this into the game c
     return false;
 }
 
-public User findUser(String playerName){
+public User findUser(String playerName){//finds a user in our players list, if it isn't found, it'll return null
     for (User i : myUsers){
         if (i.getUserName().equals(playerName)){
             return i;
@@ -53,7 +64,7 @@ public User findUser(String playerName){
     return null;
 }
 
-public void updatePlayer(User updatedUser){
+public void updatePlayer(User updatedUser){//updates the players stats and details if they change anything
     for (User i : myUsers){
         if (i.getUserName().equals(updatedUser.getUserName())){
             i.changeUserPassword(updatedUser.getUserPassword());
@@ -63,12 +74,18 @@ public void updatePlayer(User updatedUser){
     }
 }
 
-public void savePlayers(){
+public void savePlayers(){//saves the players to the txt file
     //filewriter
 
 }
 
-void getTop10Players(){
+public void printPlayers(){//prints the players
+    for (User i : myUsers){
+        System.out.println(i);
+    }
+}
+
+void getTop10Players(){//top 10 leaderboard
     //comparable, make a leaderboard, make it look cool
 
 }
