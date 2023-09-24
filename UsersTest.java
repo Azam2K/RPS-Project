53e220fb34ded3d39346946c174667e8b1b0b5fb
Azam <azamuseless@gmail.com>
2023-09-24 16:50:37 +0100

    Start of testing Users

---
+++ UsersTest.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+
+public class UsersTest {
+
+    Users users = new Users();
+
+    @Test
+    void testAddPlayer() {
+
+    }
+
+    @Test
+    void testDeletePlayer() {
+
+    }
+
+    @Test
+    void testFindUser() {
+
+    }
+
+    @Test
+    void testGetLeaderboard() {
+
+    }
+
+    @Test
+    void testPrintPlayers() {
+
+    }
+
+    @Test
+    void testSavePlayers() {
+
+    }
+
+    @Test
+    void testUpdatePlayer() {
+
+    }
+}
